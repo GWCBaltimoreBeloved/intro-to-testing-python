eaf176b2b9289485879d38fc6e7e6914eca3957b
Phoebe Taylor <phoebe3410@gmail.com>
2022-08-20 11:43:32 -0400

    add main and test files

---
+++ main.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def moon_weight():
+    earth_weight = input("What is your weight?")
+    return int(earth_weight) / 6.05
+
+# Use kilograms instead of pounds
+
+
+if __name__ == '__main__':
+    print('result:')
+    add(2, 3)
+    # print(add(20, 30))
---
+++ test.py
@@ -0,0 +1,7 @@
+import main
+
+def test_add():
+    assert main.add(2, 3) == 5
+
+def test_add_two():
+    assert main.add(7, 3) == 10
